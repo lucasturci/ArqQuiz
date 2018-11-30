e9a4f9bdff7039859f1ca82967ea2c1230668a1e
Fernanda <fernanda.marana44@gmail.com>
2018-11-29 23:54:55 -0200

    Inicio da interface do jogo

---
+++ questions.js
@@ -0,0 +1,20 @@
+var questions = [{
+		"question": "What is 10/2?",
+		
+		"option1": "3",
+		"option2": "5",
+		"option3": "115",
+		"option4": "12",
+		
+		"answer": "5"
+	}, 	{
+		"question": "What is 30/3?",
+		
+		"option1": "3",
+		"option2": "5",
+		"option3": "10",
+		"option4": "11",
+		
+		"answer": "10"
+	}
+	];
